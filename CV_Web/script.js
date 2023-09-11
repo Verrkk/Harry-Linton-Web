a6418035005dab0b0cb61a0a8d5edb8f917810fb
Verrkk <105636549+Verrkk@users.noreply.github.com>
2023-09-11 21:28:46 +0100

    Add files via upload
    Finalised version of vapourwave website

---
+++ CV_Web/script.js
@@ -0,0 +1,38 @@
+// Logic for the navigation bar
+
+function openNav() {
+    document.getElementById('cvSidenav').style.width = "250px"; 
+}
+
+function closeNav() {
+    document.getElementById('cvSidenav').style.width = "0";
+}
+
+openNav();
+closeNav();
+
+// VARIABLES
+const elH = document.querySelectorAll(".timeline li > div");
+
+// START
+window.addEventListener("load", init);
+
+function init() {
+  setEqualHeights(elH);
+}
+
+// SET EQUAL HEIGHTS
+function setEqualHeights(el) {
+  let counter = 0;
+  for (let i = 0; i < el.length; i++) {
+    const singleHeight = el[i].offsetHeight;
+
+    if (counter < singleHeight) {
+      counter = singleHeight;
+    }
+  }
+
+  for (let i = 0; i < el.length; i++) {
+    el[i].style.height = `${counter}px`;
+  }
+}
